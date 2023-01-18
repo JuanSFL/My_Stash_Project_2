aa0ee41d6f7e31ff752bcdfae020d27a1d3a1ed4
CodingJarrett <codingjarrett@gmail.com>
2023-01-18 18:50:37 -0500

    added and edited controllers, added ENV file and fixed stashHouse.db

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/User');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -18,7 +22,11 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ 
+      where: { 
+        email: req.body.email 
+      }, 
+    });
 
     if (!userData) {
       res
